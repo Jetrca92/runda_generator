2b92801293492c7940909436b4f1e231e55c6145
Jetrca92 <blaz.guzej@gmail.com>
2023-11-30 22:32:19 +0100

    Optimize count function

--- generator.py
+++ generator.py
@@ -13,8 +13,32 @@ def read_csv():
                 'Ritem': row.get('Ritem', ''),
                 'Bas': row.get('Bas', ''),
             })
-        print(song_list)
+        count_list(song_list)
         return song_list
+    
+def count_categories(song_list, category_key, category_values):
+    category_counts = {category: 0 for category in category_values}
+
+    for item in song_list:
+        category = item.get(category_key, '').lower()
+
+        for cat_val in category_values:
+            if cat_val in category:
+                category_counts[cat_val] += 1
+                break
+
+    return category_counts
+    
+def count_list(list):
+    zasedba_values = ['kvintet', 'trio']
+    ritem_values = ['polka', 'valček']
+    bas_values = ['bas', 'bariton']
+    
+    zasedba_counts = count_categories(list, 'Zasedba', zasedba_values)
+    ritem_counts = count_categories(list, 'Ritem', ritem_values)
+    bas_counts = count_categories(list, 'Bas', bas_values)
+
+    print(f"Kvintet: {zasedba_counts['kvintet']} pesmi, Trio: {zasedba_counts['trio']} pesmi. Skupaj {[ritem_counts['polka']]} polk in {ritem_counts['valček']} valčkov. Na bas {bas_counts['bas']} komadov, na bariton {bas_counts['bariton']} komadov.")
 
 if __name__ == "__main__":
     read_csv()
