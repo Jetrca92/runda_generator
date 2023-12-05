c2a051213ee3c4c3071dab6e45460964932b1f63
Jetrca92 <blaz.guzej@gmail.com>
2023-12-05 20:47:20 +0100

    can't handle case nineight

--- advent_day_2.py
+++ advent_day_2.py
@@ -1,23 +1,35 @@
 import re
 
-number_words =["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+words_mapping = {
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9",
+}
 
+def map_digit_sequence(sequence):
+    if sequence.isdigit():
+        return sequence
+    else:
+        return words_mapping[sequence]
+    
 def read_input():
     with open("input.txt") as t:
         lines = t.readlines()
         total = 0
+        regex = "one|two|three|four|five|six|seven|eight|nine|1|2|3|4|5|6|7|8|9"
         for line in lines:
-            digits = []
-            words = []
-            
-            for char in line:
-                if char.isdigit():
-                    digits.append(char)
-            for word in number_words:
-                if word in line:
-                    words.append(word)
-            
-            print(digits, words)
+            digit_sequences = re.findall(regex, line, flags=re.IGNORECASE)
+            number_1 = map_digit_sequence(digit_sequences[0])
+            number_2 = map_digit_sequence(digit_sequences[-1])
+            number = number_1 + number_2
+            total += int(number)
+            print(number)
         print(total)
 
 if __name__ == "__main__":
